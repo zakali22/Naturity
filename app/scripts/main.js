c11842222dd28b1dd48e21474d6b1c5173b29ae7
Zakeria Ali <zak.fined@gmail.com>
2017-12-05 00:17:38 +0000

    Add the promo section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+// external js: masonry.pkgd.js
+
+	$('.grid').masonry({
+	  itemSelector: '.grid-item',
+	  columnWidth: '.grid-sizer',
+	  percentPosition: true
+	});
+
+});
