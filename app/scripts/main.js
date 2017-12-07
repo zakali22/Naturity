b0ae6fed0929b5921cb9ea8aaa0e99265862a620
Zakeria Ali <zak.fined@gmail.com>
2017-12-07 10:20:28 +0000

    Add Jumbotron section for Face-oil

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-// external js: masonry.pkgd.js
+	// Dropdown
 	$('.dropdown').on('mouseover', function(){
 		$(this).children('.dropdown-menu').slideDown('fast');
 	});
 	$('.dropdown').on('mouseleave', function(){
 		$(this).children('.dropdown-menu').slideUp('fast');
 	});
+
 });
