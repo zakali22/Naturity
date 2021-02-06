868f40b98b74fc264cb08304a68e04ecebddfb13
Zak Ali <zak.ali@thetin.net>
2021-02-06 23:50:22 +0000

    Setup project and add basic landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {ReactComponent as LogoImage} from "./assets/Logo1.svg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="temp-main">
+        <div className="temp-left-panel">
+          <header className="temp-header">
+            <a href="#"><LogoImage /></a>
+          </header>
+          <div className="temp-body">
+            <h1 className="p temp-body__heading">Coming Soon<span className="fill-green">.</span></h1>
+            <div className="temp-body__description">
+              <h2 className="temp-body__subheading">Who are we?</h2>
+              <p>We produce Natural Aromatherapy and Skincare Products We have a passion for Producing Natural Skincare and Aromatherapy products which aren't only super natural in their origins, but effective in giving you wonderful skin and a rejuvenated mind. Using botanical extracts, we take the natural properties of plants to renew, regenerate and revive and include them in our skincare products to produce highly effective, indulgent skincare products that give visible results to your skin, whilst maintaining our ethics of provenance and naturalness.</p>
+            </div>
+          </div>
+        </div>
+        <div className="temp-right-panel"></div>
+      </div>
     </div>
   );
 }
